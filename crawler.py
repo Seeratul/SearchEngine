aa050e8209ac228bbd35566f48873eb7ca29b913
Jay <jhattendorf@uni-osnabrueck.de>
2025-01-09 15:12:52 +0100

    format python files

--- crawler.py
+++ crawler.py
@@ -4,67 +4,69 @@ from whoosh.index import create_in
 from whoosh.index import open_dir
 from whoosh.fields import *
 from whoosh.qparser import QueryParser
-#from funcs import saver
 
 
-#for setup we use simple presets they are inelegant but they do the job
-prefix = 'https://vm009.rz.uos.de/crawl/'
-start_url = prefix+'index.html'
+# For setup we use simple presets they are inelegant but they do the job
+prefix = "https://vm009.rz.uos.de/crawl/"
+start_url = prefix + "index.html"
 agenda = [start_url]
-#We create the schema in which woosh will store data
-schema = Schema(url =TEXT(stored=True),title = TEXT(stored=True), body = KEYWORD,excerpt =TEXT(stored=True))
+# We create the schema in which woosh will store data
+schema = Schema(
+    url=TEXT(stored=True),
+    title=TEXT(stored=True),
+    body=KEYWORD,
+    excerpt=TEXT(stored=True),
+)
 ix = create_in("indexdir", schema)
 
-#Predefine saver to help with code legibility
-def saver(soup,index,url):
-    #Extract and cut to size various parts of the soup
+
+# Predefine saver to help with code legibility
+def saver(soup, index, url):
+    # Extract and cut to size various parts of the soup
     title = soup.html.title.text
     body = soup.html.p.text
     writer = index.writer()
-    excerpt =  soup.html.p.text
+    excerpt = soup.html.p.text
     excerpt = excerpt[0:50]
-    #Save the extracted parts
-    writer.add_document(url = url,title= title, body = body+" "+title, excerpt = excerpt) 
+    # Save the extracted parts
+    writer.add_document(url=url, title=title, body=body + " " + title, excerpt=excerpt)
     writer.commit()
     return
 
-#Loop for as long as there is something on the agenda
+
+# Loop for as long as there is something on the agenda
 while agenda:
 
-    #Grab the top object from the agenda
-    url = agenda.pop()  
-    #Create a requests object
+    # Grab the top object from the agenda
+    url = agenda.pop()
+    # Create a requests object
     r = requests.get(url)
- 
-    #Check wheter the target page is reachable
+
+    # Check wheter the target page is reachable
     if r.status_code == 200:
-        
-        #Extracts the soup from the page and saves it into our woosh library
-        soup = bs(r.text,features= "lxml")
-        saver(soup,ix,url)
 
-        #look for further links to crawl
-        for link in soup.find_all('a'):
-            string = link.get('href')
+        # Extracts the soup from the page and saves it into our woosh library
+        soup = bs(r.text, features="lxml")
+        saver(soup, ix, url)
 
-            #ensusure found links are html links
+        # look for further links to crawl
+        for link in soup.find_all("a"):
+            string = link.get("href")
+
+            # ensusure found links are html links
             if ".html" in string:
 
-                #ensures that links with and without the proper prefix get added
+                # ensures that links with and without the proper prefix get added
                 if prefix in string:
                     newurl = string
                 else:
-                    newurl= prefix+string
+                    newurl = prefix + string
 
-                #Checks wheter the link already as been visited or is alredy in agenda
+                # Checks wheter the link already as been visited or is alredy in agenda
                 query = QueryParser("url", ix.schema).parse(string)
                 with ix.searcher() as searcher:
                     results = searcher.search(query)
-                    
+
                 if len(results) == 0:
-                    if (newurl in agenda)== False:
+                    if (newurl in agenda) == False:
                         agenda.append(newurl)
-
-
-
-            
--- search.py
+++ search.py
@@ -3,36 +3,39 @@ from whoosh import index
 from whoosh.qparser import QueryParser
 import numpy as np
 
-#open woosh index
-ix = index.open_dir("indexdir") 
+# open woosh index
+ix = index.open_dir("indexdir")
 
 # Retrieving data
 
-#flask --app search run
+# flask --app search run
 
 app = Flask(__name__)
 
-#landing page
+
+# landing page
 @app.route("/")
 def start():
-    return render_template('start.html')
+    return render_template("start.html")
 
-#search page
+
+# search page
 @app.route("/search")
 def search():
-    #Instantiate hits
+    # Instantiate hits
     hits = [""]
-    #open the searcher
+    # open the searcher
     with ix.searcher() as searcher:
 
-    # find entries containing the query
+        # find entries containing the query
         print(type("q"))
         query = QueryParser("body", ix.schema).parse(request.args["q"])
         results = searcher.search(query)
-        #extract data from the individual results while iterating over them
+        # extract data from the individual results while iterating over them
         for i in range(len(results)):
-            hits.append([results[i]["url"],[results[i]["title"]],[results[i]["excerpt"]]])
-            
-    #Hand the extratcted data over to the template
-    return render_template("searched.html", rev= hits, len = len(results))
+            hits.append(
+                [results[i]["url"], [results[i]["title"]], [results[i]["excerpt"]]]
+            )
 
+    # Hand the extratcted data over to the template
+    return render_template("searched.html", rev=hits, len=len(results))
